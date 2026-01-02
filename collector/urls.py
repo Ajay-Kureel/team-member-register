f7647732246cb1d50d41e72cc2bfc0810a66596e
Ajay-Kureel <73892679+Ajay-Kureel@users.noreply.github.com>
2026-01-03 01:06:26 +0530

    urls

--- collector/urls.py
+++ collector/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
-
+from .views import create_superuser
 urlpatterns = [
     path('', views.home, name='home'),
+    path("create-superuser/", create_superuser),
     path('fill/', views.fill_form, name='fill_form'),
     path('review/', views.review_person, name='review_person'),
+    path('export/', views.export_excel, name='export_excel'),
+
 ]
